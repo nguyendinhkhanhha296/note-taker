cc42d455a4cabb49c64b17d5562abe07dfd311ed
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-08-28 12:02:07 -0700

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const api = require('./routes/index.js');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const app = express();
